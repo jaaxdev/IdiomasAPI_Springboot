4947eb8d4369a3f9d30838a1b4a1d62886cc51d8
jaaziel <jaaziel.irc@gmail.com>
2021-01-24 21:46:41 -0600

    first commit

---
+++ src/main/kotlin/com/jaax/idiomasapi/IdiomasapiApplication.kt
@@ -0,0 +1,11 @@
+package com.jaax.idiomasapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IdiomasapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<IdiomasapiApplication>(*args)
+}
---
+++ src/test/kotlin/com/jaax/idiomasapi/IdiomasapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jaax.idiomasapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IdiomasapiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
